965619e4344a147f30cd2c7379978be484ed6de1
Anwesh43 <anweshthecool0@gmail.com>
2020-07-04 05:05:34 +0530

    adding constants and libraries

--- linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
+++ linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.linepushorpullview
 /**
  * Created by anweshmishra on 04/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val foreColor : Int = Color.parseColor("#2196F3")
