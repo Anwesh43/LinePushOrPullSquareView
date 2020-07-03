3fb1e6b878a3a55ec7109a8a1508e146c36f1d48
Anwesh43 <anweshthecool0@gmail.com>
2020-07-04 05:16:56 +0530

    adding LinePushOrPullView class

--- linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
+++ linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
@@ -52,3 +52,19 @@ fun Canvas.drawLPOPSNode(i : Int, scale : Float, paint : Paint) {
     drawLinePushOrPullSquare(i, w, gap, scale, paint)
     restore()
 }
+
+class LinePushOrPullSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouch(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
