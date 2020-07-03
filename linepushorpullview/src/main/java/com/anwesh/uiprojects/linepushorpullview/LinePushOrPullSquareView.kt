0c443aeb12b0bfed82b66b1058ebecb7760a5216
Anwesh43 <anweshthecool0@gmail.com>
2020-07-04 05:26:52 +0530

    adding LPOPSNode class

--- linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
+++ linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
@@ -115,4 +115,45 @@ class LinePushOrPullSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LPOPSNode(var i : Int, val state : State = State()) {
+
+        private var next : LPOPSNode? = null
+        private var prev : LPOPSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LPOPSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPOPSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LPOPSNode {
+            var curr : LPOPSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
