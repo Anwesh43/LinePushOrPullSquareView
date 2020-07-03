6154489bc81f7b4c0a1773a544a90c1191edd19a
Anwesh43 <anweshthecool0@gmail.com>
2020-07-04 05:15:40 +0530

    adding Canvas extension method

--- linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
+++ linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
@@ -26,3 +26,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLinePushOrPullSquare(i : Int, w : Float, gap : Float, scale : Float, paint : Paint) {
+    val size : Float = gap / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sj : Float = 1f - 2 * (i % 2)
+    drawLine(0f, 0f, (w / 2 - size / 2) * (sf1 + sf2 * sj), 0f, paint)
+    save()
+    translate((w / 2 - size / 2) * sj * sf2, 0f)
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    restore()
+}
+
+fun Canvas.drawLPOPSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawLinePushOrPullSquare(i, w, gap, scale, paint)
+    restore()
+}
