61b6d490b2002820d80d6ae79039b9948bfc90d1
Anwesh43 <anweshthecool0@gmail.com>
2020-07-04 05:32:59 +0530

    adding renderer in view

--- linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
+++ linepushorpullview/src/main/java/com/anwesh/uiprojects/linepushorpullview/LinePushOrPullSquareView.kt
@@ -179,4 +179,27 @@ class LinePushOrPullSquareView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LinePushOrPullSquareView) {
+
+        private val animator : Animator = Animator(view)
+        private val lpops : LinePushOrPullSquare = LinePushOrPullSquare(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lpops.draw(canvas, paint)
+            animator.animate {
+                lpops.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lpops.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
